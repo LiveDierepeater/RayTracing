f0580fad2e9a94e1e1b677e50701fcee50296f1a
LiveDierepeater <81476895+LiveDierepeater@users.noreply.github.com>
2025-03-26 20:25:45 +0100

    Implemented Emissive Materials
    video: https://www.youtube.com/watch?v=AbVfW4X01a0&list=PLlrATfBNZ98edc5GshdBtREv5asFW3yXl&index=13

--- RayTracing/src/WalnutApp.cpp
+++ RayTracing/src/WalnutApp.cpp
@@ -25,11 +25,25 @@ public:
 		blueSphere.Albedo = { 0.1f, 0.05f, 0.85f };
 		blueSphere.Roughness = 0.35f;
 
+		Material& orangeSphere = m_Scene.Materials.emplace_back();
+		orangeSphere.Albedo = { 0.8f, 0.5f, 0.2f };
+		orangeSphere.Roughness = 0.1f;
+		orangeSphere.EmissionColor = orangeSphere.Albedo;
+		orangeSphere.EmissionPower = 2.0f;
+
 		{
 			Sphere sphere;
 			sphere.Position = { 0.0f, 0.5f, -2.0f };
 			sphere.Radius = 1.0f;
 			sphere.MaterialIndex = 0;
+			m_Scene.Spheres.push_back(sphere);
+		}
+
+		{
+			Sphere sphere;
+			sphere.Position = { 2.0f, 0.5f, -2.0f };
+			sphere.Radius = 1.0f;
+			sphere.MaterialIndex = 2;
 			m_Scene.Spheres.push_back(sphere);
 		}
 
@@ -87,6 +101,8 @@ public:
 			ImGui::ColorEdit3("Albedo", glm::value_ptr(material.Albedo));
 			ImGui::DragFloat("Roughness", &material.Roughness, 0.05f, 0, 1);
 			ImGui::DragFloat("Metallic", &material.Metallic, 0.05f, 0, 1);
+			ImGui::ColorEdit3("Emission Color", glm::value_ptr(material.EmissionColor));
+			ImGui::DragFloat("Emission Power", &material.EmissionPower, 0.05f, 0.0f, FLT_MAX);
 
 			ImGui::Separator();
 
